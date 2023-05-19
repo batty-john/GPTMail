d89b399ec368119bb3708b61cfa8f973a6beffc9
John Batty <login@johnbatty.me>
2023-05-19 09:10:11 -0600

    Adding login, sign-up, and chaning imap library.

--- app.js
+++ app.js
@@ -1,42 +1,66 @@
 const express = require('express');
 const http = require('http');
-const notifier = require('mail-notifier');
+const imaps = require('imap-simple');
 const path = require('path');
-const mysql = require('mysql2');
+const mailparser = require('mailparser').simpleParser;
 
 const app = express();
+const server = http.createServer(app);
 
 app.set('view engine', 'ejs');
-app.set('public', path.join(__dirname, 'public'));
+app.set('views', path.join(__dirname, 'public'));
 
-let newMail = new Array();
+let newMail = [];
 
+const config = {
+    imap: {
+        user: "morgan.thacker@ducimus.digital",
+        password: "0$v(mZD+T#_^",
+        host: "atlas.ducimus.digital",
+        port: 993,
+        tls: true,
+        authTimeout: 3000
+    }
+};
 
-const server = http.createServer(app);
-//app.use(express.static('public'));
+imaps.connect(config).then(function(connection) {
+  return connection.openBox('INBOX').then(function() {
+      var searchCriteria = ['ALL'];
+      var fetchOptions = {
+          bodies: ['HEADER', 'TEXT'],
+          struct: true,
+          markSeen: false
+      };
+      return connection.search(searchCriteria, fetchOptions);
+  }).then(function(messages) {
+      const promises = messages.map(function(message) {
+          if (message.attributes.struct) { // check if struct is defined
+              const allParts = imaps.getParts(message.attributes.struct);
+              const part = allParts.find(part => part.which === "TEXT");
+              return connection.getPartData(message, part)
+                  .then(function(partData) {
+                      return mailparser(partData);
+                  });
+          } else {
+              // If struct is not defined, we return a resolved Promise
+              // with some placeholder object
+              return Promise.resolve({subject: "No Subject", text: "No Text"});
+          }
+      });
 
-app.get('/', (req, res) => {
-    var examplemail = "this" + " is " + "a " +  "test email";
-    res.render('index.ejs', {mail:newMail});
+      return Promise.all(promises);
+  }).then(function(emails) {
+      emails.forEach(function(email) {
+          console.log(email);
+          newMail.push(email);
+      });
+  });
 });
 
-const imap = {
-    user: "morgan.thacker@ducimus.digital",
-    password: "0$v(mZD+T#_^",
-    host: "mail.ducimus.digital",
-    port: 993, // imap port
-    tls: true,
-    tlsOptions: { rejectUnauthorized: false }
-  };
-  
-  notifier(imap)
-    .on('mail', mail => console.log(mail))
-    .on('error', err => console.log(err))
-    .start();
+app.get('/', (req, res) => {
+    res.render('index.ejs', {mail: newMail});
+});
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+server.listen(3000, () => console.log('Server listening on port 3000'));
 
  
